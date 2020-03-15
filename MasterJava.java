253702d4037132c3fddb563a50571fd67bf2118e
neni5 <sheth.niral9@gmail.com>
2020-03-15 14:50:12 +0530

    MasterJava added new line

--- MasterJava.java
+++ MasterJava.java
@@ -1,5 +1,6 @@
 public class MasterJava {
 	public static void main(String a[]) {
-		System.out.println("Doing some greate stuff.");
+		System.out.println("Doing some great stuff.");
+		System.err.println("Error doing great stuff.");
 	}
 }
